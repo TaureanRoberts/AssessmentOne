a9da084873d9b2b567048e9f54995a3f46e75b6e
Taurean Roberts <taureanjroberts@gmail.com>
2017-10-12 00:33:48 -0500

    string complete

--- StringClass/main.cpp
+++ StringClass/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include "Strings.h"
 
+using namespace std;
+
 int main()
 {
 	MyString testOne = "Johnny";
@@ -12,7 +14,7 @@ int main()
 	//testOne.AppendStrings(testTwo);
 	//testOne.PrependStrings(testTwo);
 	//char a = testOne.GetIndex(2);
-	int b = testOne.GetInput();
-
+	int b = testOne.GetInput(2);
+	istream& operator >> (istream& testOne, MyString &testTwo );
 	system("pause");
 }
