82695f23d7745778b6104c3179452cbd18e6bcf6
Taurean Roberts <taureanjroberts@gmail.com>
2017-10-12 00:51:50 -0500

    .

--- StringClass/main.cpp
+++ StringClass/main.cpp
@@ -14,7 +14,7 @@ int main()
 	//testOne.AppendStrings(testTwo);
 	//testOne.PrependStrings(testTwo);
 	//char a = testOne.GetIndex(2);
-	int b = testOne.GetInput(2);
+	//int b = testOne.GetInput(2);
 	istream& operator >> (istream& testOne, MyString &testTwo );
 	system("pause");
 }
